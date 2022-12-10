18911a3dd4f56642343aec3eecbd6620f4e86e02
David Tarasenko <davidtarasenko38@gmail.com>
2022-12-10 23:57:24 +0300

    Разработка загрузки аккаунтов. Есть проблемы с int.

--- main_file.c
+++ main_file.c
@@ -1,46 +1,62 @@
 #include <stdio.h>
-#define NUMBER_OF_USERS 3
 
+int number_of_accounts = 0;     // для хранения кол-ва аккаунтов
+int number_of_participants = 0; // для хранения кол-ва участников
+FILE *accounts_file;            // ссылка на файл с аккаунтами
+FILE *competitors_file;         // ссылка на файл с участниками конкурса
 
+static struct account // структура для пользователей программой
+    {
+        char login[20];    
+        char password[20]; 
+        int role; // 1 - администратор,  0 - обычный пользователь 
+    };
+    static struct account account_information[10]; // делается глобальной
+
+int loading_accounts(void);
 
 int main(void) {
+    
     char login[50];
     char password[50];
 
-     struct account
-     {
-        char login[20];
-        char password[20];
-        int role; // 1 - администратор,  0 - обычный пользователь 
-     } user[NUMBER_OF_USERS];
+    static struct participant // структура для участника
+    {
+        char fio[50];      // ФИО участника
+        int year_of_birth; // год рождения
+        char tool[20];     // инструмент
+        int place;         // место
+    };
+    static struct participant participant_information[50]; // делается глобальной для облегчения работы функций
 
-     /*user[0].login = "Mindowg";
-     user[0].password = "1195-1263";
-     user[0].role = 0;*/
-
-    
-     user[2].login[0] = 'T';
-     user[2].password = "password";
-     user[2].role = 1;
-
-    FILE *f;
-
-    if (!(f = fopen("competitors_1.txt", "r+t"))) { // открытие файла в режиме чтения и записи
-        printf("Невозможно открыть файл \n");
+    if (!(accounts_file = fopen("accounts.txt", "r+t"))) { // открытие файла в режиме чтения и записи
+        printf("Не удальлось открыть файл с аккаунтами.\n");
+        return 0;
     } else {
-        printf("Файл открыт или создан");
+        printf("Файл с аккаунтами открыт успешно. \n");
+        //loading_accounts();
     }
 
-    while(1) { // основное тело программы
-        printf("Введите пожалуйста логин \n");
-        gets(login);
-        for (int i = 0; i < NUMBER_OF_USERS; i++) {
-            if (user[i].login == login) {
-                printf("Введите пожалуйста пароль \n");
-                gets(password);
-            }
-        }
+    if (!(competitors_file = fopen("competitors.txt", "r+t"))) { // открытие файла в режиме чтения и записи
+        printf("Не удалось открыть файл c участниками конкурса.\n");
+        return 0;
+    } else {
+        printf("Файл с участниками конкурса открыт успешно. \n");
     }
+    
 
     return 0;
+}
+
+int loading_accounts(void) {
+    int i = 0;
+    char place_char; // для того чтобы записать место, а потом перевести в int
+    while(feof(accounts_file)) {
+       fgets(account_information[i].login, 20, accounts_file); 
+       fgets(account_information[i].password, 20, accounts_file); 
+       fgets(place_char, 1, accounts_file); 
+       account_information[i].role = int(place_char);
+       i++;
+       number_of_accounts++;
+    }
 }
